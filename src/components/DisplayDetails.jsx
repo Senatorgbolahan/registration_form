8aa47d880fff567d8f09307d2a3983ee6ec53d0a
Senatorgbolahan <efunkoyaomogbolahan@gmail.com>
2022-02-23 17:37:52 +0100

    1 file modified

--- src/components/DisplayDetails.jsx
+++ src/components/DisplayDetails.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const DisplayDetails = ({ stores }) => {
   return (
-    <div>
-      {stores.map((store) => (
-        <p>{store.firstName}</p>,
-        <p>{store.lastName}</p>,
-        <p>{store.email}</p>
-      ))}
-    </div>
+    <>
+      {stores.map(
+        (store) => (
+          <p>{store.firstName}</p>,
+          <p>{store.lastName}</p>,
+          <p>{store.email}</p>
+        )
+      )}
+    </>
   );
 };
 
